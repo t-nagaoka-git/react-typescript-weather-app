d1dc407873a65b7ac54a9d4a74d352dfbf129ac9
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-08-29 12:57:49 +0900

    721 Form.tsx を作ろう その3

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,11 +2,18 @@ import {useState} from 'react';
 
 const Form = () => {
   const [city, setCity] = useState<string>('');
+  const getWeather = (e: any) => {
+    e.preventDefault();
+    fetch('https://api.weatherapi.com/v1/current.json?key=22b72bbfe59f475dbdf33559212908&q=London&aqi=no')
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
   return (
     <form>
       <input type="text" name="city" placeholder="都市名" onChange={(e) => setCity(e.target.value)} />
-      {city}
-      <button type="submit">Get Weather</button>
+      <button type="submit" onClick={getWeather}>
+        Get Weather
+      </button>
     </form>
   );
 };
