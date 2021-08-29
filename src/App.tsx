1d4966a0c72c16c89728d063334d468242280e79
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-08-29 14:01:39 +0900

    1304 Results.tsx を作ろう その2

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,48 @@
-import {useState} from 'react';
+import React, {useState} from 'react';
 
 import Title from './components/Title';
 import Form from './components/Form';
 import Results from './components/Results';
 import './App.css';
 
+type ResultsStateType = {
+  country: string;
+  cityName: string;
+  temperature: string;
+  conditionText: string;
+  icon: string;
+};
+
 function App() {
   const [city, setCity] = useState<string>('');
-  const getWeather = (e: any) => {
+  const [results, setResults] = useState<ResultsStateType>({
+    country: '',
+    cityName: '',
+    temperature: '',
+    conditionText: '',
+    icon: '',
+  });
+  const getWeather = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    fetch('https://api.weatherapi.com/v1/current.json?key=22b72bbfe59f475dbdf33559212908&q=London&aqi=no')
+    fetch(`https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHERAPI_API_KEY}&q=${city}&aqi=no`)
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) =>
+        setResults({
+          country: data.location.country,
+          cityName: data.location.name,
+          temperature: data.current.temp_c,
+          conditionText: data.current.condition.text,
+          icon: data.current.condition.icon,
+        })
+      );
   };
   return (
-    <div className="App">
-      <Title />
-      <Form setCity={setCity} getWeather={getWeather} />
-      <Results />
+    <div className="wrapper">
+      <div className="container">
+        <Title />
+        <Form setCity={setCity} getWeather={getWeather} />
+        <Results results={results} />
+      </div>
     </div>
   );
 }
